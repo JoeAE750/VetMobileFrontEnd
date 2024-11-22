f090deac29e466e01aadf4e765bd76e7f56a99ee
joel <joel.ae.27@gmail.com>
2024-11-22 12:24:00 -0500

    Avance Navegacion Mascotas

--- app/src/main/java/com/fisi/vetmobile/ui/view/RegistrarMascotaScreen.kt
+++ app/src/main/java/com/fisi/vetmobile/ui/view/RegistrarMascotaScreen.kt
@@ -1,17 +1,11 @@
 package com.fisi.vetmobile.ui.view
 
 import androidx.compose.foundation.layout.*
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.AccountBox
 import androidx.compose.material3.Button
-import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.ExposedDropdownMenuBox
-import androidx.compose.material3.ExposedDropdownMenuDefaults
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextField
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -24,12 +18,14 @@ import androidx.compose.ui.unit.sp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.fisi.vetmobile.data.model.Mascotas // Importa la clase Mascotas
 import com.fisi.vetmobile.ui.components.Boton_Atras
+import com.fisi.vetmobile.ui.components.SelectorTextField
 import com.fisi.vetmobile.ui.components.TextFieldFormulario
-import com.fisi.vetmobile.ui.viewmodel.LoginViewModel
 import com.fisi.vetmobile.ui.viewmodel.MascotasViewModel
 import compose.icons.FontAwesomeIcons
 import compose.icons.fontawesomeicons.Solid
 import compose.icons.fontawesomeicons.solid.Paw
+
+
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -42,7 +38,6 @@ fun RegistrarMascotaScreen(
     var raza by remember { mutableStateOf("") }
     var edad by remember { mutableStateOf("") }
     var peso by remember { mutableStateOf("") }
-    var genero by remember { mutableStateOf("") }
 
     Column(
         modifier = Modifier
@@ -74,65 +69,37 @@ fun RegistrarMascotaScreen(
         )
 
         TextFieldFormulario(value = nombre, onValueChange = { nombre = it }, label = "Nombre")
-
-        val options = listOf("Perro", "Gato")
-        var expanded by remember { mutableStateOf(false) }
-        var selectedOptionText by remember { mutableStateOf(options[0]) }
-
-        ExposedDropdownMenuBox(
-            expanded = expanded,
-            onExpandedChange = {
-                expanded = !expanded
-            }
-        ) {
-            TextField(
-                readOnly = true,
-                value = selectedOptionText,
-                onValueChange = { },
-                label = { Text("Label") },
-                trailingIcon = {
-                    ExposedDropdownMenuDefaults.TrailingIcon(
-                        expanded = expanded
-                    )
-                },
-                colors = ExposedDropdownMenuDefaults.textFieldColors()
-            )
-            ExposedDropdownMenu(
-                expanded = expanded,
-                onDismissRequest = {
-                    expanded = false
-                }
-            ) {
-                options.forEach { selectionOption ->
-                    DropdownMenuItem(
-                        text = {},
-                        onClick = {
-                            selectedOptionText = selectionOption
-                            expanded = false
-                        }
-                    )
-                }
-            }
+        var selectedValueEspecie by remember { mutableStateOf("Perro") }
+        val onValueEspecieChanged: (String) -> Unit = { newValue ->
+            selectedValueEspecie = newValue
         }
-
+        val optionsEspecie = listOf("Perro", "Gato")
+        SelectorTextField(selectedValue = selectedValueEspecie, options = optionsEspecie, label = "Especie", onValueChangedEvent = onValueEspecieChanged)
         TextFieldFormulario(value = raza, onValueChange = { raza = it }, label = "Raza")
         TextFieldFormulario(value = edad, onValueChange = { edad = it }, label = "Edad")
         TextFieldFormulario(value = peso, onValueChange = { peso = it }, label = "Peso")
-        TextFieldFormulario(value = genero, onValueChange = { genero = it }, label = "Genero")
+        var selectedValueGenero by remember { mutableStateOf("Masculino") }
+        val onValueGeneroChanged: (String) -> Unit = { newValue ->
+            selectedValueGenero = newValue
+        }
+        val optionsGenero = listOf("Masculino", "Femenino")
+        SelectorTextField(selectedValue = selectedValueGenero, options = optionsGenero, label = "Sexo", onValueChangedEvent = onValueGeneroChanged)
+
 
 
         val nuevaMascota = Mascotas(
             id_usuario = idusuario.toInt(),
             nombre = nombre,
-            especie = if(selectedOptionText == "Perro") "1" else "2",
+            especie = if(selectedValueEspecie == "Perro") "1" else "2",
             raza = raza,
             edad = edad,
             peso = peso,
-            genero = genero
+            genero = if(selectedValueGenero == "Masculino") "M" else "F"
         )
 
         Button(onClick = {
             mascotasViewModel.registrarMascota(nuevaMascota)
+            navigateUp()
         }) {
             Text("Registrar Mascota")
         }
